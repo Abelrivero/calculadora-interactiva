945d20bae995f5d727e5b8ac920514cfb00a878c
Abelrivero <139782755+Abelrivero@users.noreply.github.com>
2024-05-20 22:00:37 -0300

    Calculadora de Practica

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
-import logo from './logo.svg';
 import './App.css';
+import freeCodeCampLogo from './imagenes/logo.png';
+import Boton from './componentes/boton';
+import Pantalla from './componentes/pantalla';
+import BotonClear from './componentes/botonClear';
+import { useState } from 'react';
+import { evaluate } from 'mathjs';
 
 function App() {
+
+  const [input, setInput] = useState('');
+
+  const agregarInput = val => {
+    setInput(input + val)
+  };
+
+  const operadoresConsecutivos = /[+\-*/]{2,}/.test(input)
+  const comienzoOperadores = /^[+\-*/]/.test(input);
+  const puntosConsecutivos = /\.\./.test(input);
+
+  const calcularResultado = () => {
+    if(operadoresConsecutivos || comienzoOperadores || puntosConsecutivos){
+      alert('Expresion Incorrecta')
+      return setInput('')
+    }
+
+    if(input){
+      setInput(evaluate(input))
+    }else{
+      alert('Ingrese un Valor')
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='frecodecamp-logo-contenedor'>
+        <img src={freeCodeCampLogo}
+        className='freecodecamp-logo'
+        alt='logo de freecodecamp'/>
+      </div>
+      <div className='contenedor-calculadora'>
+        <Pantalla input={input}/>
+        <div className='fila'>
+          <Boton manejarClic={agregarInput}>1</Boton>
+          <Boton manejarClic={agregarInput}>2</Boton>
+          <Boton manejarClic={agregarInput}>3</Boton>
+          <Boton manejarClic={agregarInput}>+</Boton>
+        </div>
+        <div className='fila'>
+          <Boton manejarClic={agregarInput}>4</Boton>
+          <Boton manejarClic={agregarInput}>5</Boton>
+          <Boton manejarClic={agregarInput}>6</Boton>
+          <Boton manejarClic={agregarInput}>-</Boton>
+        </div>
+        <div className='fila'>
+          <Boton manejarClic={agregarInput}>7</Boton>
+          <Boton manejarClic={agregarInput}>8</Boton>
+          <Boton manejarClic={agregarInput}>9</Boton>
+          <Boton manejarClic={agregarInput}>*</Boton>
+        </div>
+        <div className='fila'>
+          <Boton manejarClic={calcularResultado}>=</Boton>
+          <Boton manejarClic={agregarInput}>0</Boton>
+          <Boton manejarClic={agregarInput}>.</Boton>
+          <Boton manejarClic={agregarInput}>/</Boton>
+        </div>
+        <div className='fila'>
+          <BotonClear manejarClear={() => setInput('')}>Clear</BotonClear>
+        </div>
+      </div>
     </div>
   );
 }
