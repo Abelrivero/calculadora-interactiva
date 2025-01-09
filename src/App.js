e6bb40d073ee10dba1bb7f1067e3a7ad74bb3f0c
Abelrivero <139782755+Abelrivero@users.noreply.github.com>
2025-01-08 23:45:07 -0300

    modificacion

--- src/App.js
+++ src/App.js
@@ -33,11 +33,6 @@ function App() {
 
   return (
     <div className='App'>
-      <div className='frecodecamp-logo-contenedor'>
-        <img src={freeCodeCampLogo}
-        className='freecodecamp-logo'
-        alt='logo de freecodecamp'/>
-      </div>
       <div className='contenedor-calculadora'>
         <Pantalla input={input}/>
         <div className='fila'>
